ebd707a372026bf88df99509a69411653af17999
Viktor Sjölind <viktor.sjolind@pelagicore.com>
2017-11-20 13:08:39 +0100

    Add template-service-wizard to QtCreator
    Signed-off-by: Viktor Sjölind <viktor.sjolind@pelagicore.com>

---
+++ test/scripts/can_find_template_service_wizard.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2017 Pelagicore AB
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0
+#
+
+import os
+
+
+def find_template(template_name, qtcreator_install_dir):
+    template_path = "{}/share/qtcreator/templates".format(qtcreator_install_dir)
+    return template_name in os.listdir(template_path)
+
+
+
+if __name__ == "__main__":
+    template_name = "template-service-wizard"
+    if not find_template(template_name, "/opt/qtcreator"):
+        raise Exception("Could not find template")
