e583a52a311044f947eac7718e58d797d577f289
Richard Pannek <richard.pannek@pelagicore.com>
2018-01-23 15:47:54 +0100

    Fix tests to allow SDKs without Qt
    The tests were written so that they assumed that the SDK would have
    Qt installed in it, but not all SDKs delivered by the PELUX project
    do have Qt installed, some of them are headless and don't need Qt.
    
    This patch rewrites the test framework in a way so that one can
    easier add new functionality to the SDE and write new tests for
    it.
    
    Signed-off-by: Richard Pannek <richard.pannek@pelagicore.com>

---
+++ test/stubs/sans_qmake_sdk.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 Pelagicore AB
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# SPDX-License-Identifier: MPL-2.0# Copyright (C) 2017 Pelagicore AB
+#
+
+echo "Running stubbed sdk-self-extractor"
+
+SDK_PATH=`dirname $0`
+
+echo " \
+export SDKTARGETSYSROOT=/opt/pelux_sdk/sysroots/aarch64-gnu-linux
+export PKG_CONFIG_SYSROOT_DIR=\$SDKTARGETSYSROOT
+export PKG_CONFIG_PATH=\$SDKTARGETSYSROOT/usr/lib/pkgconfig
+export CONFIG_SITE=/opt/pelux_sdk/site-config-aarch64-gnu-linux
+export OECORE_NATIVE_SYSROOT=\"/opt/pelux_sdk/sysroots/i686-oesdk-linux\"
+export OECORE_TARGET_SYSROOT=\"\$SDKTARGETSYSROOT\"
+export OECORE_ACLOCAL_OPTS=\"-I /opt/pelux_sdk/sysroots/i686-oesdk-linux/usr/share/aclocal\"
+export CC=true
+export CXX=true
+export CPP=true
+export AS=true
+export LD=true
+export GDB=true
+export STRIP=true
+export RANLIB=true
+export OBJCOPY=true
+export OBJDUMP=true
+export AR=true
+export NM=true
+export M4=m4
+export TARGET_PREFIX=true
+export QMAKESPEC=stub
+export CONFIGURE_FLAGS=\"--target=aarch64-gnu-linux --host=aarch64-gnu-linux --build=i686-linux --with-libtool-sysroot=\$SDKTARGETSYSROOT\"
+export CFLAGS=\" -O2 -pipe -g -feliminate-unused-debug-types \"
+export CXXFLAGS=\" -O2 -pipe -g -feliminate-unused-debug-types \"
+export LDFLAGS=\"-Wl,-O1 -Wl,--hash-style=gnu -Wl,--as-needed\"
+export CPPFLAGS=\"\"
+export KCFLAGS=\"--sysroot=\$SDKTARGETSYSROOT\"
+export OECORE_DISTRO_VERSION=\"11.0.0\"
+export OECORE_SDK_VERSION=\"nodistro.0\"
+export ARCH=arm64
+export CROSS_COMPILE=true
+
+mkdir ${SDK_PATH}/stub_bin
+ln -s /bin/true ${SDK_PATH}/stub_bin/cmake
+export PATH=\$PATH:${SDK_PATH}/stub_bin
+
+" > ${SDK_PATH}/environment-setup-stub
+
+touch ${SDK_PATH}/sysroot-stub
